47e5fb6bb0589c46125944762bfd7c61e3d93524
hod271 <h5j4r271@gmail.com>
2023-08-25 22:42:04 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
-# Change the following print statement so that it uses the addition operator + and prints out 8
-
+# Change the following print statement so that it uses the addition operator + and prints out 8 
+print(5 + 3)
 # Write a print statement that uses the division operator / 
-
+print ( 8 / 2 )
 # Add a print statement that uses multiplication *
-
+print ( 2 * 4 )
 # Create a variable called `length` and assign a value to it
-
+length = 5
 # Create a variable called    `width` and assign a value to it
-
+width = 3
 # Create a variable called `area` and assign to it the multiplication of `length` and `width`
-
+area = length * width 
 # Print `area` in the following: "The result is " followed by the `area` variable
-
+print (f'the result is {area}')
 
 # BONUS: Print the area in the following syntax: "If the length of a rectangle is 5, and width is 3 then the area is: 15". Keep in mind that you should replace the numbers with their respective variables. 
+print (f'is the length of the rectangle is {length} and the width is {width} then the area is {area}')
